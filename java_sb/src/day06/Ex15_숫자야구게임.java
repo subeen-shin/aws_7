7ac42fab534d09c06829ed823586d86f78f1bf5d
subeen-shin <subeen.shin@gmail.com>
2026-07-31 18:14:29 +0900

    Update Ex15_숫자야구게임.java

--- java_sb/src/day06/Ex15_숫자야구게임.java
+++ java_sb/src/day06/Ex15_숫자야구게임.java
@@ -1,5 +1,9 @@
 package day06;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex15_숫자야구게임 {
 
 	public static void main(String[] args) {
@@ -23,8 +27,127 @@ public class Ex15_숫자야구게임 {
 		 * 결과 : O
 		 * 입력 : 1 9 4
 		 * 결과 : 3S
+		 * 정답입니다.
 		 * */
 
+		//랜덤수 3개 생성(중복되지 않은, 1~9사이)
+		ArrayList<Integer> com = createRandomArray(1, 9, 3);
+		
+		System.out.println("랜덤 : " + com);
+		//3S가 될때까지 반복
+		int strike = 0, ball = 0;
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Integer> user;
+		while( strike < 3) {
+			//1~9사이 정수 3개 입력
+			System.out.println("입력 : ");
+			user = Ex14_List연습예제2.inputNums(3, 1, 9);
+			System.out.println(user);
+			
+			//결과를 출력
+			//스트라이크의 개수를 셈
+			strike = getStrike(com, user);
+			
+			//둘의 개수를 셈
+			ball = getBall(com, user);
+			
+			//스트라이크와 둘의 개수를 이용하여 결과를 출력
+			printResult(strike, ball);
+		}
+	}	
+	
+	
+
+	private static void printResult(int strike, int ball) {
+		System.out.println("결과 : ");
+		// 1S, 1S2B, 2B, O
+		//스트라이크가 0이 아니면 ?S를 출력(print)
+		if (strike != 0) {
+			System.out.print( strike + "S");
+		}
+		
+		// 볼이 0이아니면 ?B를 출력(print)
+		 if (ball != 0) {
+			System.out.print( ball + "B");
+		}
+		
+		//스트라이크가 0이고 볼도 0이면 O를 출력(print
+		if(strike != 0 && ball == 0) {
+			System.out.print("O");
+		}
+		System.out.println();
+	}
+
+
+
+	private static int getBall(ArrayList<Integer> com, ArrayList<Integer> user) {
+		int ball = 0;
+		//[1 2 3] [1 3 4] : 1S1B
+	      //3번 반복 : i는 0부터 com.size()-1까지 1씩 증가
+		for(int i = 0 ; i < com.size() -1; i++) {
+			if(com.get(i) == user.get(i)) {
+			
+		}
+	      
+	         //com에서 i번지에 있는 값을 꺼내서 user에 있으면
+			if (user.contains(com.get(i))) {
+			
+		
+	      
+	            //볼을 증가(이 때 계사된 볼은 실제 볼의 개수가 아니라 볼+스트라이크 개수)
+				ball++;
+				}
+			}
+	      //스트라이크의 개수를 구함
+			int strike = getStrike(com, user);
+	      
+	      //볼 - 스트라이크을 리턴
+		
+		
+		return ball - strike;
+		}
+	
+
+
+	private static int getStrike(ArrayList<Integer> com, ArrayList<Integer> user) {
+		//[1 2 3] [1 3 4]
+			int strike = 0;
+	  
+		  //3번 반복 : i는 1부터 com.size()-1까지 1씩 증가
+			for(int i = 0 ; i < com.size() -1; i++) {
+				if(com.get(i) == user.get(i)) {
+					
+					strike++;
+				}	
+			}
+	       
+	    //스트라이크 개수를 리턴
+		return strike;
+	}
+
+
+
+	public static ArrayList<Integer> createRandomArray(int min, int max, int size) {
+		
+		//정수 리스트를 생성
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		Scanner scan = new Scanner(System.in);
+			
+			//리스트에 size개 저장될때까지 반복
+		while(list.size() < size) {
+			
+			//min~max사이의 랜덤한 수를 생성
+			Random random = new Random();
+			int num = random.nextInt(min, max + 1);
+		
+			//랜덤수가 리스트에 없으면 : Ex14예제 참고
+			if(!list.contains(num)) {	
+			//랜덤 수를 리스트에 추가
+				list.add(num);
+			}
+		}
+		return list;
 	}
 
 }
