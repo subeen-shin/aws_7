41b49e6e0b91c571adf634cecdb6152d2bd13c36
subeen-shin <subeen.shin@gmail.com>
2026-08-21 18:17:15 +0900

    Create Post.java

---
+++ spring boot/boot_01/src/main/java/kr/fast/boot/entity/Post.java
@@ -0,0 +1,52 @@
+package kr.fast.boot.entity;
+
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post")
+@Getter 
+@NoArgsConstructor 
+@AllArgsConstructor 
+public class Post {
+	
+	@Id
+	private int id;
+	
+	private String title;
+	
+	private String content;
+	@Column(name="created_at", nullable = false)
+	private Date createdAt = new Date();
+	@Column(name="view_count")
+	private int viewCount;
+	@Column(name="up_count")
+	private int upCount;
+	@Column(name="down_count")
+	private int downCount;
+	@Column(name="is_deleted", nullable = false)
+	private char isDeleted = 'N';
+	@Column(name="member_Id")
+	private String memberId;
+	@Column(name="board_Id")
+	private int boardId;
+	
+	
+	public Post(String title, String content, String writer, Integer boardId) {
+		this.title = title;
+		this.content = content;
+		this.memberId = writer;
+		this.boardId = boardId;
+	}
+}
+
+
+	
