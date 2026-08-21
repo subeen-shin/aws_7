ad34e9b9fe2dbfb6beb03a94eb1eb3dda93c8f14
subeen-shin <subeen.shin@gmail.com>
2026-08-21 18:17:19 +0900

    Create PostController.java

---
+++ spring boot/boot_01/src/main/java/kr/fast/boot/controller/PostController.java
@@ -0,0 +1,68 @@
+package kr.fast.boot.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.fast.boot.dto.PostDTO;
+import kr.fast.boot.entity.Board;
+import kr.fast.boot.entity.Post;
+import kr.fast.boot.service.BoardService;
+import kr.fast.boot.service.PostService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/post")
+@AllArgsConstructor
+public class PostController {
+
+	private final BoardService boardService;
+
+	private final PostService postService;
+
+	@GetMapping("/board")
+	public ResponseEntity<Object> boardGet() {
+
+		// 서비스야. 게시판 목록을 가져와
+		List<Board> list = boardService.getBoardList();
+		return ResponseEntity.ok(list);
+	}
+
+	@PostMapping("")
+	public ResponseEntity<Object> post(@RequestBody PostDTO dto) {
+		// 서비스야 게시글 정보 줄테니 등록해줘.
+		// 서비스야.게시글등록해줘(게시글정보);
+		try {
+			postService.insertPost(dto);
+			return ResponseEntity.ok("게시글을 등록했습니다.");
+		} catch (Exception e) {
+			return ResponseEntity.ok(e.getMessage());
+		}
+	}
+
+	@GetMapping("")
+	public ResponseEntity<Object> get() {
+		// 게시글 목록 = 서비스야.게시글목록가져와();
+		List<Post> list = postService.getPostList();
+		return ResponseEntity.ok(list);
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> idGet(@PathVariable("id") int id) {
+		try {
+			Post post = postService.getPost(id);
+			// 게시글 목록 = 서비스야.게시글목록가져와()
+			return ResponseEntity.ok(post);
+		} catch (Exception e) {
+			return ResponseEntity.ok(null);
+		}
+
+	}
+}
