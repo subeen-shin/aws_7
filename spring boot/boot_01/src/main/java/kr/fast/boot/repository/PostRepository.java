bd26e6e8c21b910c38be2d4044e74f9da4aadda7
subeen-shin <subeen.shin@gmail.com>
2026-08-24 15:18:36 +0900

    Update PostRepository.java

--- spring boot/boot_01/src/main/java/kr/fast/boot/repository/PostRepository.java
+++ spring boot/boot_01/src/main/java/kr/fast/boot/repository/PostRepository.java
@@ -8,6 +8,6 @@ import kr.fast.boot.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Integer>{
 
-	List<Post> findAllByOrderByIdDesc();
+	List<Post> findAllByIsDeletedOrderByIdDesc(String isDelted);
 
 }
